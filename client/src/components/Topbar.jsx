e144a4890a07224b5227acb38b8ae412861573f3
Muhammad Awais <awaismahaar499@gmail.com>
2024-12-26 22:25:11 +0500

    Institute Management Mern Stack Project

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -5,8 +5,8 @@ function Topbar() {
   const {user} = useContext(UserContext)
   return (
     <div className='top-bar d-flex align-items-center gap-2'>
-    <img src={user.imageUrl}></img>
-      <h4 className='mt-2 text-white'>{user.lastName}</h4>
+    <img src={user?.imageUrl}></img>
+      <h4 className='mt-2 text-white'>{user?.lastName}</h4>
   </div>
   )
 }
